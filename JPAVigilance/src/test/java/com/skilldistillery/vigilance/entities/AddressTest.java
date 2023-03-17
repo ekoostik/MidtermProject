482f0ed7b8815fa843fc75bfbad3e423a8cb8d6c
rob.tisdale <rtisdale.bt@gmail.com>
2023-03-17 15:36:45 -0500

    replace deleted files

---
+++ JPAVigilance/src/test/java/com/skilldistillery/vigilance/entities/AddressTest.java
@@ -0,0 +1,56 @@
+package com.skilldistillery.vigilance.entities;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class AddressTest {
+
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private Address address;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("JPAVigilance");
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		emf.close();
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		address = em.find(Address.class, 1);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		em.close();
+		address = null;
+	}
+
+	@Test
+	void test_address_map() {
+		assertNotNull(address);
+		assertEquals("HI", address.getState());
+	}
+	
+//	@Test
+//	void test_neighborhood() {
+//		assertNotNull(address.getNeighborhood());
+//		assertEquals(1, address.getNeighborhood());
+//		
+//	}
+
+}
