e3c9575bc3beed0d8c433d9584faccefc92f2214
David Dunlevy <david.dunlevy@gmail.com>
2023-03-18 06:34:56 +1300

    updated entity and test

---
+++ JPAVigilance/src/test/java/com/skilldistillery/vigilance/entities/AnimalTest.java
@@ -0,0 +1,49 @@
+package com.skilldistillery.vigilance.entities;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class AnimalTest {
+
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private Animal animal;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("JPAVigilance");
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		emf.close();
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		animal = em.find(Animal.class, 1);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		em.close();
+		animal = null;
+	}
+
+	@Test
+	void test_animal_map() {
+		assertNotNull(animal);
+		assertEquals("Moose", animal.getSpecies());
+	}
+
+}
---
+++ JPAVigilance/src/test/java/com/skilldistillery/vigilance/entities/PersonOfInterestTest.java
@@ -0,0 +1,49 @@
+package com.skilldistillery.vigilance.entities;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class PersonOfInterestTest {
+
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private PersonOfInterest personOfInterest;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("JPAVigilance");
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		emf.close();
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		personOfInterest = em.find(PersonOfInterest.class, 1);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		em.close();
+		personOfInterest = null;
+	}
+
+	@Test
+	void test_POI_mapping() {
+		assertNotNull(personOfInterest);
+		assertEquals('P', personOfInterest.getDescription().charAt(0));
+	}
+
+}
